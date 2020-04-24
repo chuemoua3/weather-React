83d4a13336c4584882f002ee1f3630ca281a6e6e
Chue <chuemoua12@outlook.com>
2020-04-24 16:38:27 -0400

    working degree and city

--- src/App.js
+++ src/App.js
@@ -1,32 +1,91 @@
-import React from 'react';
+import React, { Component } from "react";
+import "./App.css";
+import Moment, { parseZone } from "moment";
+import "moment-timezone";
 
-const API_KEY = "5354b50c1d9aa695a926bb4fc5de683b";
+class App extends Component {
+  state = {
+  
+  };
 
-//Remaking a new file because i deleted some files on here by accident
-getWeather = async (e) => {
-  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-const data = await api_call.json();
-if (city && country) {
-  this.setState({
+  getWeather = () => {
+    const inputZip = document.getElementById("inputZip").value;
+    console.log(typeof inputZip);
 
-  });
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?zip=" + inputZip + ",us&appid=" +
+        process.env.REACT_APP_OW_API_KEY + "&units=imperial"
+    )
+      .then((response) => {
+        if (response.status !== 200) {
+          console.log(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
+          return;
+        }
 
-function App() {
-  return(
-    <div className = "app">
-      <main>
-        <div className = "search-box">
-          <input type="text" id="search"className="search-bar"placeholder="Enter Zip Code..."></input>
-        </div>
-        <div className = "location-box">
-          <div className = "location">Test Location, Location</div>
-          <div className = "date">Test Date, Date</div>
-        </div>
-      </main>
-    </div>
-  )
-   
+        // Examine the text in the response
+        response.json().then((data) => {
+          console.log(data);
+          this.setState({
+            temp: data.main.temp,
+            feels_like: data.main.feels_like,
+            city: data.name,
+            timezone: data.timezone,
+          
+          })
+          console.log(this.state);
+        });
+      })
+      .catch(function (err) {
+        console.log("Fetch Error :-S", err);
+      });
+
+      this.getTime();
+  }
+  
+  getTime = () =>{
+    var moment = Moment();
+    var tz = this.state.timezone;
+    var currentTime = moment.tz(tz).format('dddd, MMMM Do YYYY, h:mm:ss a');
+    this.setState({
+      time:currentTime,
+    });
+    // var current = Moment().utcOffset(tz).format('MMMM Do YYYY, h:mm:ss a');
+    // //current = current.utcOffset(tz);
+    // console.log(current)
+    // this.setState({
+    //   time: current,
+    // })
+  }
+
+  // handleClick(){
+  //   let zipInput = document.getElementById("zipInput").value;
+  //   this.setState({zip: zipInput});
+  //   console.log(this.state.zip)
+  //   //this.getWeather();
+  // }
+
+  render() {
+    return (
+      <>
+      <div>
+        <input type = "text" placeholder="Enter Zip Code..." id="inputZip"></input>
+        <button onClick={this.getWeather}>Search</button>
+      </div>
+      <div>
+        <h5 id="temp">{this.state.temp}</h5>
+        <h5 id="city">{this.state.city}</h5>
+        <h5>{this.state.time}</h5>
+      </div>
+      </>
+    ); 
+  }
 }
+
 export default App;
-​
+
+
+
+
 
