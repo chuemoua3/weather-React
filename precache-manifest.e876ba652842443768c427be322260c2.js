abc2c84cbb6b2e249fe120714f19b5a4fa2cb6bc
Chue <chuemoua12@outlook.com>
2020-04-26 21:18:47 -0400

    Updates

---
+++ precache-manifest.e876ba652842443768c427be322260c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa806b5ff4b9ee313c979183f36dafe9",
+    "url": "/weather-React/index.html"
+  },
+  {
+    "revision": "f747c895b050f1805feb",
+    "url": "/weather-React/static/css/main.cc96477f.chunk.css"
+  },
+  {
+    "revision": "71813fe0a06535907c79",
+    "url": "/weather-React/static/js/2.ba9506a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-React/static/js/2.ba9506a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f747c895b050f1805feb",
+    "url": "/weather-React/static/js/main.d208f816.chunk.js"
+  },
+  {
+    "revision": "bfdc0caf8b2660aa0f0f",
+    "url": "/weather-React/static/js/runtime-main.482d7f95.js"
+  }
+]);
